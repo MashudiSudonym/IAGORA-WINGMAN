62e81c9c4801d347afb96b22c1cc373f7b0b985a
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2022-04-20 19:37:36 +0700

    remove get ssid

--- app/src/main/java/com/iagora/wingman/auth/ssid/domain/repository/WingmanSSIDRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.iagora.wingman.auth.ssid.domain.repository
-
-import com.iagora.wingman.common.util.Resource
-import com.iagora.wingman.common.util.SimpleResource
-import kotlinx.coroutines.flow.Flow
-
-interface WingmanSSIDRepository {
-    suspend fun getWingmanSSID(): String?
-}
