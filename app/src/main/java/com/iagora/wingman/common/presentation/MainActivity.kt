c187878302306306e0a2ec0f9baf0870cae70a1b
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2022-04-21 22:53:45 +0700

    theming, assets

--- app/src/main/java/com/iagora/wingman/common/presentation/MainActivity.kt
+++ app/src/main/java/com/iagora/wingman/common/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.iagora.wingman.common.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -34,7 +35,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column {
+        Text(text = "H1 $name!", style = MaterialTheme.typography.h1)
+        Text(text = "H2 $name!", style = MaterialTheme.typography.h2)
+        Text(text = "H3 $name!", style = MaterialTheme.typography.h3)
+        Text(text = "H4 $name!", style = MaterialTheme.typography.h4)
+        Text(text = "H5 $name!", style = MaterialTheme.typography.h5)
+        Text(text = "H6 $name!", style = MaterialTheme.typography.h6)
+        Text(text = "Body1 $name!", style = MaterialTheme.typography.body1)
+        Text(text = "Body2 $name!", style = MaterialTheme.typography.body2)
+        Text(text = "Subtitle1 $name!", style = MaterialTheme.typography.subtitle1)
+        Text(text = "Subtitle2 $name!", style = MaterialTheme.typography.subtitle2)
+        Text(text = "Button $name!", style = MaterialTheme.typography.button)
+        Text(text = "Caption $name!", style = MaterialTheme.typography.caption)
+        Text(text = "Overline $name!", style = MaterialTheme.typography.overline)
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/iagora/wingman/common/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/iagora/wingman/common/presentation/ui/theme/Type.kt
@@ -2,27 +2,114 @@ package com.iagora.wingman.common.presentation.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.iagora.wingman.R
+
+
+// Poppins font family
+val PoppinsFontFamily = FontFamily(
+    Font(R.font.poppins_black, FontWeight.Black),
+    Font(R.font.poppins_black_italic, FontWeight.Black, FontStyle.Italic),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.poppins_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.poppins_extra_bold_italic, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(R.font.poppins_extra_light, FontWeight.ExtraLight),
+    Font(R.font.poppins_extra_light_italic, FontWeight.ExtraLight, FontStyle.Italic),
+    Font(R.font.poppins_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_semi_bold, FontWeight.SemiBold),
+    Font(R.font.poppins_semi_bold_italic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.poppins_thin, FontWeight.Thin),
+    Font(R.font.poppins_thin_italic, FontWeight.Thin, FontStyle.Italic)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    h1 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 96.sp,
+        letterSpacing = (-1.5).sp
+    ),
+    h2 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 60.sp,
+        letterSpacing = (-0.5).sp
+    ),
+    h3 = TextStyle(
+        fontFamily = PoppinsFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
+        fontSize = 48.sp,
+        letterSpacing = 0.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 34.sp,
+        letterSpacing = 0.25.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        letterSpacing = 0.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        letterSpacing = 0.15.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        letterSpacing = 0.15.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        letterSpacing = 0.1.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 0.25.sp
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        letterSpacing = 1.25.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = PoppinsFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 12.sp,
+        letterSpacing = 0.4.sp
+    ),
+    overline = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp,
+        letterSpacing = 1.5.sp
     )
-    */
 )
