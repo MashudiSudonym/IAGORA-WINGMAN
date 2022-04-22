5b2a8441c3689fea956b931058c3b04d36519769
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2022-04-22 19:16:13 +0700

    create use case for verify otp and save token and user id from the return of verify otp endpoint

--- app/src/main/java/com/iagora/wingman/auth/otp/domain/use_case/verify_otp_use_case/VerifyOTPWithSaveCredentialsUseCase.kt
+++ app/src/main/java/com/iagora/wingman/auth/otp/domain/use_case/verify_otp_use_case/VerifyOTPWithSaveCredentialsUseCase.kt
@@ -1,10 +1,10 @@
 package com.iagora.wingman.auth.otp.domain.use_case.verify_otp_use_case
 
 import com.iagora.wingman.auth.credential.domain.repository.CredentialDataStorePreferencesRepository
-import com.iagora.wingman.auth.otp.domain.model.VerifyOTPResponse
 import com.iagora.wingman.auth.otp.domain.repository.OTPRepository
 import com.iagora.wingman.common.util.Resource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class VerifyOTPWithSaveCredentialsUseCase(
     private val otpRepository: OTPRepository,
@@ -13,7 +13,35 @@ class VerifyOTPWithSaveCredentialsUseCase(
     suspend operator fun invoke(
         phoneNumber: String,
         otpCode: String
-    ): Flow<Resource<VerifyOTPResponse>> {
-        return otpRepository.verifyOTP(phoneNumber, otpCode)
+    ): Flow<Resource<Boolean>> {
+        val verifyOTP = otpRepository.verifyOTP(phoneNumber, otpCode)
+
+        return flow {
+            verifyOTP.collect { result ->
+                when (result) {
+                    is Resource.Error -> {
+                        emit(Resource.Loading(true))
+                        emit(Resource.Success(false))
+                        emit(Resource.Loading(false))
+                    }
+                    is Resource.Loading -> {
+                        emit(Resource.Loading(true))
+                        emit(Resource.Loading(false))
+                    }
+                    is Resource.Success -> {
+                        emit(Resource.Loading(true))
+
+                        val token = result.data?.result?.refreshToken
+                        val userId = result.data?.result?.wingmanId
+
+                        credentialDataStorePreferencesRepository.setToken(token ?: "")
+                        credentialDataStorePreferencesRepository.setUserId(userId ?: "")
+
+                        emit(Resource.Success(true))
+                        emit(Resource.Loading(false))
+                    }
+                }
+            }
+        }
     }
 }
