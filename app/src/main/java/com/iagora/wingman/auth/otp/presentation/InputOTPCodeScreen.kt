299abe17cfa470dc88b24387c9ca6edb94282114
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2022-04-22 17:14:34 +0700

    initial input otp code screen

---
+++ app/src/main/java/com/iagora/wingman/auth/otp/presentation/InputOTPCodeScreen.kt
@@ -0,0 +1,10 @@
+package com.iagora.wingman.auth.otp.presentation
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Composable
+@Destination
+fun InputOTPCodeScreen() {
+    
+}
