69977ef6956d1ffeff959524785cf8013ccda2ef
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2022-04-22 18:57:11 +0700

    create use case for verify otp and save token and user id from the return of verify otp endpoint

--- app/src/main/java/com/iagora/wingman/auth/otp/di/UseCaseModule.kt
+++ app/src/main/java/com/iagora/wingman/auth/otp/di/UseCaseModule.kt
@@ -1,8 +1,9 @@
 package com.iagora.wingman.auth.otp.di
 
+import com.iagora.wingman.auth.credential.domain.repository.CredentialDataStorePreferencesRepository
 import com.iagora.wingman.auth.otp.domain.repository.OTPRepository
 import com.iagora.wingman.auth.otp.domain.use_case.send_otp_use_case.SendOTPUseCase
-import com.iagora.wingman.auth.otp.domain.use_case.verify_otp_use_case.VerifyOTPUseCase
+import com.iagora.wingman.auth.otp.domain.use_case.verify_otp_use_case.VerifyOTPWithSaveCredentialsUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -22,7 +23,10 @@ object UseCaseModule {
 
     @Provides
     @Singleton
-    fun provideVerifyOTPUseCase(otpRepository: OTPRepository): VerifyOTPUseCase {
-        return VerifyOTPUseCase(otpRepository)
+    fun provideVerifyOTPWithSaveCredentialUseCase(
+        otpRepository: OTPRepository,
+        credentialDataStorePreferencesRepository: CredentialDataStorePreferencesRepository
+    ): VerifyOTPWithSaveCredentialsUseCase {
+        return VerifyOTPWithSaveCredentialsUseCase(otpRepository, credentialDataStorePreferencesRepository)
     }
 }
--- app/src/main/java/com/iagora/wingman/auth/otp/domain/use_case/verify_otp_use_case/VerifyOTPWithSaveCredentialsUseCase.kt
+++ app/src/main/java/com/iagora/wingman/auth/otp/domain/use_case/verify_otp_use_case/VerifyOTPWithSaveCredentialsUseCase.kt
@@ -1,11 +1,15 @@
 package com.iagora.wingman.auth.otp.domain.use_case.verify_otp_use_case
 
+import com.iagora.wingman.auth.credential.domain.repository.CredentialDataStorePreferencesRepository
 import com.iagora.wingman.auth.otp.domain.model.VerifyOTPResponse
 import com.iagora.wingman.auth.otp.domain.repository.OTPRepository
 import com.iagora.wingman.common.util.Resource
 import kotlinx.coroutines.flow.Flow
 
-class VerifyOTPUseCase(private val otpRepository: OTPRepository) {
+class VerifyOTPWithSaveCredentialsUseCase(
+    private val otpRepository: OTPRepository,
+    private val credentialDataStorePreferencesRepository: CredentialDataStorePreferencesRepository
+) {
     suspend operator fun invoke(
         phoneNumber: String,
         otpCode: String
